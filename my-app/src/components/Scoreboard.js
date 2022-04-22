ad5a70930d5068f9d9dfa146e02efed298b71fa7
sichi-o <54566593+sichi-o@users.noreply.github.com>
2022-04-22 15:14:38 -0500

    more comments

--- my-app/src/components/Scoreboard.js
+++ my-app/src/components/Scoreboard.js
@@ -16,22 +16,22 @@ class Scoreboard extends React.Component {
         const own_score = this.state.own_score; 
         const opp_score = this.state.opp_score;
 
+        // This calculates your own score
         socketio.on("hit_to_client", (data) => {
-            console.log("hey i got this!!");
             this.setState({
                 own_score: data.username.score
             }) 
-
-            console.log(data.username.name + "'s own score is: " + data.username.score);
         });
 
+        // This calculates the opponents score
+        socketio.removeAllListeners("sub_to_client");
         socketio.on("sub_to_client", (data) => {
             this.setState({
                 opp_score: data.username.score
             }) 
-            console.log(data.username.name + "'s own score is hmmm: " + data.username.score);
         })
 
+        // If the game has started then show the scoreboard
         if(this.props.start){
             return(
                 <div className='scoreboard' key={"scoreboard"}>
